6e4cedff950a4c41d87c81d02a335263df56c8bb
Wokgui <149906755+Wokgui@users.noreply.github.com>
2026-08-13 22:43:02 +0200

    Charger les corrections UI v84

--- middleware.js
+++ middleware.js
@@ -1,8 +1,19 @@
-export default function middleware(request) {
-  const url = new URL('/layout-air-v80.css', request.url);
-  return Response.redirect(url, 307);
+export default async function middleware(request) {
+  const path=new URL(request.url).pathname;
+  if(path==='/layout-air-v79.css'){
+    const url=new URL('/layout-air-v80.css',request.url);
+    return Response.redirect(url,307);
+  }
+  if(path==='/'){
+    const url=new URL('/index.html',request.url);
+    const response=await fetch(url,{headers:request.headers});
+    let html=await response.text();
+    html=html.replace('</body>','<script src="/ui-patch-v84.js?v=20260813-84"></script></body>');
+    const headers=new Headers(response.headers);
+    headers.set('content-type','text/html; charset=utf-8');
+    headers.set('cache-control','no-cache');
+    return new Response(html,{status:response.status,headers});
+  }
 }
 
-export const config = {
-  matcher: '/layout-air-v79.css',
-};
+export const config={matcher:['/','/layout-air-v79.css']};
