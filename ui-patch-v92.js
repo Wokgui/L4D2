e8f71ea6be2d56a75a4571a5fd3dd98b0eaa4868
Wokgui <149906755+Wokgui@users.noreply.github.com>
2026-08-14 23:48:45 +0200

    Agrandir le rond Steam de Nouveautés

--- ui-patch-v92.js
+++ ui-patch-v92.js
@@ -10,35 +10,35 @@
       position:absolute!important;
       left:50%!important;
       top:50%!important;
-      width:160%!important;
-      height:160%!important;
+      width:205%!important;
+      height:205%!important;
       max-width:none!important;
       max-height:none!important;
       transform:translate(-50%,-50%)!important;
       object-fit:contain!important;
     }
     .draw .res.home-res .welcome-actions .news .welcome-steam-icon:after{
-      right:-11px!important;
-      bottom:-8px!important;
+      right:-13px!important;
+      bottom:-9px!important;
     }
     @media(max-width:420px){
       .draw .res.home-res .welcome-actions .news .welcome-steam-icon img{
-        width:160%!important;
-        height:160%!important;
+        width:205%!important;
+        height:205%!important;
       }
       .draw .res.home-res .welcome-actions .news .welcome-steam-icon:after{
-        right:-10px!important;
-        bottom:-7px!important;
+        right:-12px!important;
+        bottom:-8px!important;
       }
     }
     @media(max-height:720px){
       .draw .res.home-res .welcome-actions .news .welcome-steam-icon img{
-        width:160%!important;
-        height:160%!important;
+        width:205%!important;
+        height:205%!important;
       }
       .draw .res.home-res .welcome-actions .news .welcome-steam-icon:after{
-        right:-9px!important;
-        bottom:-6px!important;
+        right:-11px!important;
+        bottom:-7px!important;
       }
     }
   `;
