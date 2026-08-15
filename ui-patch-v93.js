078625e405912108e2be11c3297c24d6313e0e84
Wokgui <149906755+Wokgui@users.noreply.github.com>
2026-08-15 09:58:30 +0200

    Uniformise espace entre fiche tirage et menu bas

--- ui-patch-v93.js
+++ ui-patch-v93.js
@@ -1,6 +1,12 @@
 (()=>{
   const s=document.createElement('style');
   s.textContent=`
+    html body .draw.page.on{
+      padding-bottom:0!important;
+    }
+    html body .draw .res:not(.home-res){
+      padding-bottom:4.5px!important;
+    }
     html body .draw .res:not(.home-res) .result-card.has-last-played .result-content{
       padding-bottom:4.5px!important;
     }
@@ -8,6 +14,9 @@
       margin-bottom:0!important;
     }
     @media(max-height:720px){
+      html body .draw .res:not(.home-res){
+        padding-bottom:3px!important;
+      }
       html body .draw .res:not(.home-res) .result-card.has-last-played .result-content{
         padding-bottom:3px!important;
       }
