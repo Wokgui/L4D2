35139849ae38199054c29e5a6f08381dc77a245e
Wokgui <149906755+Wokgui@users.noreply.github.com>
2026-08-15 09:55:20 +0200

    Uniformiser l'espace inférieur de la fiche

---
+++ ui-patch-v93.js
@@ -0,0 +1,17 @@
+(()=>{
+  const s=document.createElement('style');
+  s.textContent=`
+    html body .draw .res:not(.home-res) .result-card.has-last-played .result-content{
+      padding-bottom:4.5px!important;
+    }
+    html body .draw .res:not(.home-res) .result-card.has-last-played .last-played-inline{
+      margin-bottom:0!important;
+    }
+    @media(max-height:720px){
+      html body .draw .res:not(.home-res) .result-card.has-last-played .result-content{
+        padding-bottom:3px!important;
+      }
+    }
+  `;
+  document.head.appendChild(s);
+})();
