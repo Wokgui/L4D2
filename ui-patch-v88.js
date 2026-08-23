17e57638f3a52dc952c67ca07023f51e625e5bfb
Wokgui <149906755+Wokgui@users.noreply.github.com>
2026-08-23 14:25:51 +0200

    Load campaign form and Steam chat icon patch

--- ui-patch-v88.js
+++ ui-patch-v88.js
@@ -185,7 +185,8 @@
     ['/ui-patch-v89.js','20260814-89'],
     ['/ui-patch-v90.js','20260814-95'],
     ['/ui-patch-v91.js','20260814-96'],
-    ['/ui-patch-v92.js','20260815-94']
+    ['/ui-patch-v92.js','20260815-94'],
+    ['/ui-patch-v96.js','20260823-1']
   ];
   let chain=Promise.resolve();
   scripts.forEach(([src,v])=>{
